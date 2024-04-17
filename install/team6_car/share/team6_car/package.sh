706da2a0ebfca6f2bba1b90b952d331cc523568f
Adrien Salvador <adrienjoelsalvador@gmail.com>
2024-04-17 15:50:00 -0700

    CMAKE cannot find the file (line 25). Need to fix.

--- install/team6_car/share/team6_car/package.sh
+++ install/team6_car/share/team6_car/package.sh
@@ -79,6 +79,7 @@ _colcon_package_sh_source_script() {
 
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/team6_car/hook/cmake_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/team6_car/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/team6_car/local_setup.sh"
 
 unset _colcon_package_sh_source_script
