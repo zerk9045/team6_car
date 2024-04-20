482f5ef457e2112cf0b7977424e3da3d11a7c23e
Adrien Salvador <adrienjoelsalvador@gmail.com>
2024-04-19 23:51:12 -0700

    Velocity Subscriber Node is working

--- src/velocity_to_pwm.cpp
+++ src/velocity_to_pwm.cpp
@@ -1,5 +1,8 @@
 
 // Include the C++ standard library headers
+#include <chrono>
+#include <functional>
+#include <string>
 #include <memory> // Dynamic memory management
  
 // Dependencies
@@ -7,42 +10,51 @@
 #include "std_msgs/msg/string.hpp" // Handles String messages in ROS 2
 #include "geometry_msgs/msg/twist_stamped.hpp"
 
-using std::placeholders::_1;
- 
-class VelocitySubscriber : public rclcpp::Node
+
+class VelocitySubscriberNode : public rclcpp::Node
 {
-  public:
-    // Constructor
-    // The name of the node is minimal_subscriber
-    VelocitySubscriber()
-    : Node("velocity_subscriber")
-    {
-      // Create the subscription.
-      // The topic_callback function executes whenever data is published
-      // to the 'addison' topic.
-      subscription_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
-      "velocity_subscriber", 10, std::bind(&VelocitySubscriber::topic_callback, this, _1));
-    }
- 
-  private:
-    // Receives the String message that is published over the topic
-    void topic_callback(const geometry_msgs::msg::TwistStamped msg) const
-    {
-      // Write the message that was received on the console window
-      //RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    }
-    // Declare the subscription attribute
-    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr subscription_;
+public:
+  VelocitySubscriberNode()
+  : Node("velocity_subscriber_node")
+  {
+    // Subscribe to the /cmd_vel_stamped topic
+    cmd_vel_stamped_subscriber_ =
+      this->create_subscription<geometry_msgs::msg::TwistStamped>(
+      "/cmd_vel_stamped",
+      rclcpp::SensorDataQoS(),
+      std::bind(&VelocitySubscriberNode::cmd_vel_stamped_callback, this, std::placeholders::_1));
+  }
+
+private:
+
+
+  // Cmd Vel Stamped subscription callback
+  void cmd_vel_stamped_callback(
+    const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+  {
+    // Linear Velocity Changed
+    //if (velocity_cmd->twist.angular.z){
+    //RCLCPP_INFO(this->get_logger(), "I heard");
+    //}
+
+    RCLCPP_INFO(this->get_logger(), "I heard: '%f, %f, %f'", msg->twist.linear.x, msg->twist.linear.y, msg->twist.linear.z);
+
+  }
+
+  // Cmd Vel StampedSubscriber
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr cmd_vel_stamped_subscriber_;
 };
- 
+
+
+
 int main(int argc, char * argv[])
 {
-  // Launch ROS 2
+  // Launch ROS 2  
   rclcpp::init(argc, argv);
-   
-  // Prepare to receive messages that arrive on the topic
-  rclcpp::spin(std::make_shared<VelocitySubscriber>());
-   
+
+  // Prepare to receive messages that arrive on the topic  
+  rclcpp::spin(std::make_shared<VelocitySubscriberNode>());
+
   // Shutdown routine for ROS2
   rclcpp::shutdown();
   return 0;
