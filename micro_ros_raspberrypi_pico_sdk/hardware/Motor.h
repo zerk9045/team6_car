12c4a8e615304044537fe4f7b76a04d2bc6c2f8f
rbretmounet <mrquacksxd@gmail.com>
2024-04-27 11:17:53 -0700

    Fixing make errors

--- micro_ros_raspberrypi_pico_sdk/hardware/Motor.h
+++ micro_ros_raspberrypi_pico_sdk/hardware/Motor.h
@@ -24,7 +24,7 @@ private:
     int pwmPin; // PWM pin for motor speed control
     int inAPin; // INA pin for motor direction control
     int inBPin; // INB pin for motor direction control
-    IRSensor* irSensor; // IRSensor object for speed calculation
+    //IRSensor* irSensor; // IRSensor object for speed calculation
     // Private helper function to set motor direction
     void updateDirection(bool inAValue, bool inBValue);
 };
