caea3a366b91ecd5d82a637f962657cfd18f64d1
Adrien Salvador <adrienjoelsalvador@gmail.com>
2024-04-26 23:03:25 -0700

    Converted to bicycle steering and added microROS hardware interface for ros_control

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             description='Use sim time if true'),
 
         node_robot_state_publisher,
