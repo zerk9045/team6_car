834d90988ff215fd3278ef92c2e5c61cc63f025b
rbretmounet <mrquacksxd@gmail.com>
2024-04-27 08:52:40 -0700

    Changed to new pins

--- micro_ros_raspberrypi_pico_sdk/config/pin_config.h
+++ micro_ros_raspberrypi_pico_sdk/config/pin_config.h
@@ -2,10 +2,10 @@
 #define TEAM6_CAR_PIN_CONFIG_H
 
 // Define the pin configuration for the Pico
-const int MOTOR_PWM = 10; // Used for RC Car motor control
-const int SERVO_PWM = 11; // Used for RC Car turning servo
-const int INA_PIN = 12; // Used to configure motor direction
-const int INB_PIN = 13; // Used to configure motor direction
-const int IR_SENSOR_PIN = 28; // Used for IR sensor D0 signal
+const int MOTOR_PWM = 21; // Used for RC Car motor control
+const int SERVO_PWM = 20; // Used for RC Car turning servo
+const int INA_PIN = 18; // Used to configure motor direction
+const int INB_PIN = 19; // Used to configure motor direction
+const int IR_SENSOR_PIN = 22; // Used for IR sensor D0 signal
 
 #endif //TEAM6_CAR_PIN_CONFIG_H
