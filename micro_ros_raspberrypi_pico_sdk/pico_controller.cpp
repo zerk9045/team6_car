3707a77da2fbe20765af977bc6631d85a50c5a66
rbretmounet <mrquacksxd@gmail.com>
2024-04-29 16:54:29 -0700

    Testing servo control

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -33,14 +33,14 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 void subscription_callback(const void * msgin)
 {
     const std_msgs__msg__String * msg = (const std_msgs__msg__String *)msgin;
-    std::string data(msg->data.data);
-    std::size_t pos = data.find("=");
-
-    int pwm = 0; // Declare pwm here
-    if (pos != std::string::npos) {
-        std::string pwmStr = data.substr(pos + 1); // Get the part after "="
-        pwm = std::stoi(pwmStr); // Convert to int
-    }
+//    std::string data(msg->data.data);
+//    std::size_t pos = data.find("=");
+    int pwm = std::stoi(msg->data.data);
+//    int pwm = 0; // Declare pwm here
+//    if (pos != std::string::npos) {
+//        std::string pwmStr = data.substr(pos + 1); // Get the part after "="
+//        pwm = std::stoi(pwmStr); // Convert to int
+//    }
 
     //servo.setAngle(pwm);
     motor.setSpeed(pwm);
