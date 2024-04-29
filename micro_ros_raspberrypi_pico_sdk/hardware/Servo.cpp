8494f6df0a758cc973b4521424ae7c456758fb03
rbretmounet <mrquacksxd@gmail.com>
2024-04-29 16:14:03 -0700

    Testing servo control

--- micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
+++ micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
@@ -38,7 +38,7 @@ void Servo::setAngle(int anglePWM) {
     // Set PWM duty cycle for servo control pin//1500000
     // Convert anglePWM to duty cycle percentage (assuming MAX_ANGLE_PWM is the maximum PWM value)
     float duty_cycle = (float)anglePWM / MAX_ANGLE_PWM;
-    if ( THRESHOLD <= abs(currAnglePWM-anglePWM)) {
+    if ( THRESHOLD >= abs(currAnglePWM-anglePWM)) {
         currAnglePWM = anglePWM;
         return;
     }
