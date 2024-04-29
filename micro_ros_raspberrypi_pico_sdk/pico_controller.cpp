0d5f209451c360dc32f27c54b9ec3a19ddb7a8b8
rbretmounet <mrquacksxd@gmail.com>
2024-04-29 16:44:04 -0700

    Testing servo control

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -119,7 +119,7 @@ int main()
   rclc_executor_init(&executor, &support.context, 2, &allocator);
   rclc_executor_add_timer(&executor, &timer);
   rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA);
-
+  motor.setSpeed(1600000);
   while(1){
     rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
   }
