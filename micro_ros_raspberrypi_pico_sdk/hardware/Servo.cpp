02507f2fb4f46bcddb05895e9daf26751fba2fae
rbretmounet <mrquacksxd@gmail.com>
2024-04-29 14:49:56 -0700

    Testing servo control

--- micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
+++ micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
@@ -10,34 +10,7 @@
 
 Servo::Servo(){
     gpio_init(SERVO_PWM);
-
-    //Setup up PWM t
-    gpio_set_function(SERVO_PWM, GPIO_FUNC_PWM);
-    pwm_set_gpio_level(SERVO_PWM, 0);
-    uint slice_num = pwm_gpio_to_slice_num(SERVO_PWM);
-
-    // Get clock speed and compute divider for 50 hz
-    uint32_t clk = clock_get_hz(clk_sys);
-    uint32_t div = clk / (20000 * 100);
-
-    // Check div is in range
-    if ( div < 1 ){
-        div = 1;
-    }
-    if ( div > 255 ){
-        div = 255;
-    }
-
-    pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, (float)div);
-
-    // Set wrap so the period is 20 ms
-    pwm_config_set_wrap(&config, 20000);
-
-    // Load the configuration
-    pwm_init(slice_num, &config, false);
-
-    pwm_set_enabled(slice_num, true);
+    set_pwm_pin(SERVO_PWM, 100, 0);
 }
 
 Servo::~Servo() {
