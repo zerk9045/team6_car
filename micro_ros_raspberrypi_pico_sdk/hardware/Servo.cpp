ba4391d73133a5961be5f40899ec196029d9469e
rbretmounet <mrquacksxd@gmail.com>
2024-04-29 14:03:41 -0700

    Testing servo control

--- micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
+++ micro_ros_raspberrypi_pico_sdk/hardware/Servo.cpp
@@ -43,9 +43,9 @@ void Servo::setAngle(int anglePWM) {
 
 int Servo::getAngle() {
     // convert the pwm signal to an angle
-    int rangePWM = MAX_ANGLE_PWM - MIN_ANGLE_PWM;
-    float scale = 2.0 / rangePWM; // Adjust the scale to map to the range -1 to 1
-    int normalizedAngle = ((currAnglePWM - MIN_ANGLE_PWM) * scale) - 1; // Subtract 1 to shift the range to -1 to 1
+//    int rangePWM = MAX_ANGLE_PWM - MIN_ANGLE_PWM;
+//    float scale = 2.0 / rangePWM; // Adjust the scale to map to the range -1 to 1
+//    int normalizedAngle = ((currAnglePWM - MIN_ANGLE_PWM) * scale) - 1; // Subtract 1 to shift the range to -1 to 1
 
-    return normalizedAngle;
+    return currAnglePWM;
 }
