9f725691843f688800da3eedc78fc523ccb753b1
rbretmounet <mrquacksxd@gmail.com>
2024-05-09 13:33:46 -0700

    fixed errors

--- micro_ros_raspberrypi_pico_sdk/hardware/IRSensor.cpp
+++ micro_ros_raspberrypi_pico_sdk/hardware/IRSensor.cpp
@@ -18,28 +18,28 @@ absolute_time_t timer;
 alarm_pool_t *timer_pool;
 // Interrupt handler for the timer
 
-//int64_t timer_interrupt(alarm_id_t id, void *user_data) {
-//    IRSensor::resetSensorInterrupts();
-//    return 100000;
-//}
+int64_t timer_interrupt(alarm_id_t id, void *user_data) {
+    IRSensor::resetSensorInterrupts();
+    return 1000000;
+}
 
 IRSensor::IRSensor() {
     gpio_init(IR_SENSOR_PIN);
     gpio_set_dir(IR_SENSOR_PIN, GPIO_IN);
     gpio_set_irq_enabled_with_callback(IR_SENSOR_PIN, GPIO_IRQ_EDGE_FALL, true, IRSensor::do_interrupt);
     // Initialize the hardware timer
-    //timer = make_timeout_time_ms(1000);
-    //add_alarm_in_us(100000, timer_interrupt, NULL, true);
+    timer = make_timeout_time_ms(1000);
+    add_alarm_in_us(1000000, timer_interrupt, NULL, true);
 }
 
-//void IRSensor::resetSensorInterrupts() {
-//    counts_per_timer = sensor_interrupts;
-//    sensor_interrupts = 0;
-//    //printf("Interrupts: %d\n", counts_per_timer);
-//}
+void IRSensor::resetSensorInterrupts() {
+    counts_per_timer = sensor_interrupts;
+    sensor_interrupts = 0;
+    //printf("Interrupts: %d\n", counts_per_timer);
+}
 
 int IRSensor::getCountsPerTimer() {
-    return sensor_interrupts;
+    return counts_per_timer;
 }
 
 void IRSensor::do_interrupt(uint gpio, uint32_t events) {
