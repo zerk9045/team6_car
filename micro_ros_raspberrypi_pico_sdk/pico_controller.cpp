3525f09a4eb6398040b52473d1a8d67a3aeac01f
rbretmounet <mrquacksxd@gmail.com>
2024-05-21 15:05:35 -0700

    Changed order of test

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -86,6 +86,9 @@ void subscription_callback_motor(const void *msgin) {
 
 int main()
 {
+    //Tests
+    testMotor();
+    testPIDController();
     stdio_init_all();
     rmw_uros_set_custom_transport(
             true,
@@ -153,9 +156,7 @@ int main()
   rclc_executor_add_timer(&executor, &timer);
   rclc_executor_add_subscription(&executor, &motor_subscriber, &msg, &subscription_callback_motor, ON_NEW_DATA);
   rclc_executor_add_subscription(&executor, &servo_subscriber, &msg, &subscription_callback_servo, ON_NEW_DATA);
-//Tests
-  testMotor();
-  testPIDController();
+
   while(1){
     rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
   }
