313caa28d7792369dfdd21af2706e3eb209b14dd
rbretmounet <mrquacksxd@gmail.com>
2024-05-22 13:54:21 -0700

    Adjusting PID value

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -94,7 +94,7 @@ void subscription_callback_motor(const void *msgin) {
 
     // If the direction has not changed and the pwm is the same, no need to update
     if (motor.getDirection() == direction) {
-        double Kp = 1.0; // Proportional gain need to tweak this value
+        double Kp = 0.3; // Proportional gain need to tweak this value
         // Measure the current speed
         double current_speed = motor.getSpeed();
         double current_pwm = 0;
