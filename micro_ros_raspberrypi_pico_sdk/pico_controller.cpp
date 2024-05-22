7d1b16865799259433cc3801a429c923891b5a08
rbretmounet <mrquacksxd@gmail.com>
2024-05-21 17:30:44 -0700

    Added average filter to working code

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -30,44 +30,9 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
         msg.data.size = strlen(msg.data.data);
         msg.data.capacity = msg.data.size + 1;
         rcl_publish(&publisher, &msg, NULL);
-        //printf("Published: '%s'\n", msg.data.data);
+        printf("Published: '%s'\n", msg.data.data);
         free(msg.data.data); // Free the allocated memory
     }
-}
-//Test the motor
-void testMotor(){
-    motor.setSpeed(1);
-    sleep_ms(1000);
-    motor.setSpeed(0.0);
-    sleep_ms(1000);
-    motor.setSpeed(-1);
-    sleep_ms(1000);
-    motor.setSpeed(0.0);
-}
-
-//Function to test the PID controller
-void testPIDController() {
-    motor.getSpeed();
-    // Set a desired speed
-    double desiredSpeed = 1;
-    motor.pidController(desiredSpeed);
-
-    // Wait for the motor to reach the desired speed
-    sleep_ms(500);
-
-    // Get the actual speed of the motor
-    double actualSpeed = motor.getSpeed();
-
-    // Print the desired and actual speeds
-    printf("Desired Speed: %f, Actual Speed: %f\n", desiredSpeed, actualSpeed);
-
-    // Repeat the process for a different desired speed
-    motor.getSpeed();
-    desiredSpeed = -1;
-    motor.pidController(desiredSpeed);
-    sleep_ms(500);
-    actualSpeed = motor.getSpeed();
-    printf("Desired Speed: %f, Actual Speed: %f\n", desiredSpeed, actualSpeed);
 }
 
 // TODO: add error checks to ensure the message is in the correct format
@@ -78,18 +43,52 @@ void subscription_callback_servo(const void * msgin) {
 
 }
 
+bool isValidDirection(const std::string& direction) {
+    return direction == "forward" || direction == "reverse" || direction == "stop";
+}
+
+bool isValidPwm(int pwm) {
+    return pwm >= 1375000 && pwm <= 3000000;
+}
+
 void subscription_callback_motor(const void *msgin) {
     const std_msgs__msg__String *msg = (const std_msgs__msg__String *)msgin;
-    double speed = std::stod(msg->data.data);
+    std::string msg_data = msg->data.data;
+
+    // Find the position of the space character
+    size_t space_pos = msg_data.find(' ');
+    if (space_pos == std::string::npos) {
+        // Invalid message format, handle error or return
+        return;
+    }
+
+    // Extract pwmValue and direction
+    int pwm = std::stoi(msg_data.substr(0, space_pos));
+    std::string direction = msg_data.substr(space_pos + 1);
+
+    // Validate direction and pwm
+    if (!isValidDirection(direction) || !isValidPwm(pwm)) {
+        // Invalid direction or pwm value, handle error or return
+        return;
+    }
+
+    // If the direction has not changed and the pwm is the same, no need to update
+    if (motor.getDirection() == direction) {
+        motor.setSpeed(pwm);
+        return;
+    }
+
+    // Update the motor direction
+    bool forward = (direction == "forward");
+    bool reverse = (direction == "reverse");
+    motor.updateDirection(forward, reverse, direction);
+
     // Set the motor speed
-    motor.setSpeed(speed);
-//    motor.getSpeed();
-//    motor.pidController(speed);
+    motor.setSpeed(pwm);
 }
 
 int main()
 {
-
     stdio_init_all();
     rmw_uros_set_custom_transport(
             true,
@@ -112,64 +111,60 @@ int main()
         return ret;
     }
 
-  rclc_support_t support;
+    rclc_support_t support;
 
-  // create init_options
-  rclc_support_init(&support, 0, NULL, &allocator);
+    // create init_options
+    rclc_support_init(&support, 0, NULL, &allocator);
 
-  // create rcl_node
-  rcl_node_t node;
-  rclc_node_init_default(&node, "pico_node", "", &support);
+    // create rcl_node
+    rcl_node_t node;
+    rclc_node_init_default(&node, "pico_node", "", &support);
 
-  // create a publisher
-  rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-    "control_topic");
+    // create a publisher
+    rclc_publisher_init_default(
+            &publisher,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+            "control_topic");
 
 
-  // create a timer,
-  rcl_timer_t timer;
-  const unsigned int timer_timeout = 100;
-  rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback);
+    // create a timer,
+    rcl_timer_t timer;
+    const unsigned int timer_timeout = 100;
+    rclc_timer_init_default(
+            &timer,
+            &support,
+            RCL_MS_TO_NS(timer_timeout),
+            timer_callback);
 
-  // create a subscriber for the motor
-  rclc_subscription_init_default(
-    &motor_subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-    "pi_motor_publishing_topic");
-  //Create a subscriber for the servo
-  rclc_subscription_init_default(
-    &servo_subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-    "pi_servo_publishing_topic");
+    // create a subscriber for the motor
+    rclc_subscription_init_default(
+            &motor_subscriber,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+            "pi_motor_publishing_topic");
+    //Create a subscriber for the servo
+    rclc_subscription_init_default(
+            &servo_subscriber,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+            "pi_servo_publishing_topic");
 
-  // create executor
-  rclc_executor_t executor;
-  rclc_executor_init(&executor, &support.context, 3, &allocator);
-  rclc_executor_add_timer(&executor, &timer);
-  rclc_executor_add_subscription(&executor, &motor_subscriber, &msg, &subscription_callback_motor, ON_NEW_DATA);
-  rclc_executor_add_subscription(&executor, &servo_subscriber, &msg, &subscription_callback_servo, ON_NEW_DATA);
-    //Tests
-    //testMotor();
-//    testPIDController();
-//    motor.setSpeed(1.0);
+    // create executor
+    rclc_executor_t executor;
+    rclc_executor_init(&executor, &support.context, 3, &allocator);
+    rclc_executor_add_timer(&executor, &timer);
+    rclc_executor_add_subscription(&executor, &motor_subscriber, &msg, &subscription_callback_motor, ON_NEW_DATA);
+    rclc_executor_add_subscription(&executor, &servo_subscriber, &msg, &subscription_callback_servo, ON_NEW_DATA);
 
-  while(1){
-    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-  }
+    while(1){
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+    }
 
-  // free resources
-  rcl_publisher_fini(&publisher, &node);
+    // free resources
+    rcl_publisher_fini(&publisher, &node);
     rcl_timer_fini(&timer);
     rcl_subscription_fini(&motor_subscriber, &node);
-  rcl_subscription_fini(&servo_subscriber, &node);
-  rcl_node_fini(&node);
+    rcl_subscription_fini(&servo_subscriber, &node);
+    rcl_node_fini(&node);
 }
