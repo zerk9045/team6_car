e9eb612c8d3a6370f6967050785bf9186767c10e
rbretmounet <mrquacksxd@gmail.com>
2024-05-22 15:23:27 -0700

    tweak d gain

--- micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
+++ micro_ros_raspberrypi_pico_sdk/pico_controller.cpp
@@ -95,7 +95,7 @@ void subscription_callback_motor(const void *msgin) {
     // If the direction has not changed and the pwm is the same, no need to update
     if (motor.getDirection() == direction) {
         double Kp = 0.2; // Proportional gain need to tweak this value
-        double Kd = 2.5; // Derivative gain, tweak this value
+        double Kd = 1.9; // Derivative gain, tweak this value
         // Measure the current speed
         double current_speed = motor.getSpeed();
         double current_pwm = 0;
